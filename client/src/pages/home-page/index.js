0b9408d4f544786835b7d68a425264ef31aa5d20
Beth Koulyras <bethkoulyras@gmail.com>
2019-08-11 01:19:36 +1000

    Updated homepage and typography styles

--- client/src/pages/home-page/index.js
+++ client/src/pages/home-page/index.js
@@ -12,7 +12,9 @@ import PushBtn from '../../components/pwa/push-btn';
 import Feedback from '../../components/common/feedback';
 import Alert from '../../components/common/alert';
 import Loading from '../../components/common/loading';
-import MainTextInput from '../../components/MainTextInput';
+import InputBase from '@material-ui/core/InputBase';
+import Button from '@material-ui/core/Button';
+
 //------------------------------------------------------------------------------
 // STYLE:
 //------------------------------------------------------------------------------
@@ -25,17 +27,38 @@ const Json = styled.pre`
 //------------------------------------------------------------------------------
 const HomePage = ({ curUser }) => (
   <div>
-    <Typography align="center" variant="h2">Home Page</Typography>
+    {/* <Typography align="center" variant="h2">Home Page</Typography> */}
     <div className="mb1" />
-    <Typography variant="h2">I am looking for a</Typography>
-    <MainTextInput placeholder="specialist" />
-    <Typography variant="h2">around this</Typography>
-    <MainTextInput placeholder="suburb" />
+    <div className="homeText">
+    <div className="initialText">
+      <Typography variant="h4" className="headingInitialText">medical services recommended by people you trust - us.</Typography>
+      <Typography variant="h5" className="secondaryInitialText">As LGBTQ+ individuals we can face difficulties finding services that get who we are.</Typography>
+      <Typography variant="h5" className="secondaryInitialText">Whether you’ve had a hard time with a healthcare provider before or you’ve never known where to look.</Typography>
+      <Typography variant="h5" className="secondaryInitialText">We are the place to start.</Typography>
+    
+    </div>
+
+    
+      <Typography variant="h2">I am looking for a</Typography>
+      <InputBase fullWidth classes={{input: "maintextinput"}} placeholder="specialist" />
+      <Typography variant="h2" className="secondaryText">around this</Typography>
+      <InputBase fullWidth classes={{input: "maintextinput"}} placeholder="suburb" />
+      <Button className="searchbtn" color="primary" size="large" variant="contained" fullWidth={true} >
+          Search
+        </Button>
+      <Typography variant="h3" align="center" className="orText">or</Typography>
+      <Button className="searchbtn" color="secondary" size="large" variant="contained" fullWidth={true} >
+          Leave a recommendation
+        </Button>
+    </div>
+
+
     <Json>
       {JSON.stringify(curUser, null, 2)}
     </Json>
     <div className="mb2" />
     <LogoutBtn />
+
     <div className="mb2" />
     <PWABtnProps>
       {(pwaBtnProps) => {
--- client/src/theme/mui.js
+++ client/src/theme/mui.js
@@ -1,10 +1,50 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import red from '@material-ui/core/colors/red';
 import green from '@material-ui/core/colors/green';
+import { Input } from '@material-ui/core';
 
 const theme = createMuiTheme({
+  spacing: 8,
+
   typography: {
     useNextVariants: true,
+    h2: {
+      fontFamily: 'SF Compact Display',
+      fontWeight: 'Bold',
+      fontSize: '32px',
+      lineHeight: '38px',
+
+    },
+    h3: {
+      fontFamily: 'SF Compact Display',
+      fontWeight: 'Semibold',
+      fontSize: '18px',
+      lineHeight: '38px',
+    },
+
+    h4: {
+      fontFamily: 'SF Compact Display',
+      fontWeight: 'Semibold',
+      fontSize: '18px',
+      lineHeight: '27px',
+    },
+
+    h5: {
+      fontFamily: 'SF Compact Display',
+      fontWeight: 'Regular',
+      fontSize: '12px',
+      lineHeight: '18px',
+    }
+
+  
+  },
+  palette: {
+    primary: {
+      main: '#5651FF'
+    },
+    secondary: {
+      main: '#F2F3F6'
+    },
   },
   switch: {
     colorPrimary: green[500],
